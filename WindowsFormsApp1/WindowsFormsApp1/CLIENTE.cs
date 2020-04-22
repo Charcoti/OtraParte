60a253cd2da310c92f0fa4ccd10bf1e39f2f2da2
Charcoti <62609129+Charcoti@users.noreply.github.com>
2020-04-22 03:20:27 -0400

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/CLIENTE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class CLIENTE
+    {
+        //rut --- nombre --- direccion
+        public string nombre;
+        public string direccion;
+        public int rut;
+    }
+}
